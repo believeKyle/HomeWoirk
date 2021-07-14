923aa8b405352f625937af5e2124496c7c6b94f4
BelieveKyle <BelieveKyle@gmail.com>
2021-07-14 18:07:55 +0800

    homework03

---
+++ HomeWork03/src/main/java/netty/OutBoundHandler.java
@@ -0,0 +1,142 @@
+package netty;
+
+import filter.HeaderHttpRequestFilter;
+import filter.HttpRequestFilter;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class OutBoundHandler {
+
+    private HttpRequestFilter filter = new HeaderHttpRequestFilter();
+
+    private CloseableHttpClient httpclient;
+    private ExecutorService proxyService;
+    private List<String> backendUrls;
+
+    public OutBoundHandler(List<String> backends) {
+
+        // this.backendUrls = backends.stream().map(this::formatUrl).collect(Collectors.toList());
+
+        httpclient = HttpClients.createDefault();
+    }
+
+    /*@Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx, "hello,kimmking");
+            } else {
+                handlerTest(fullRequest, ctx, "hello,others");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }*/
+
+    public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, HttpRequestFilter filter) {
+        String backendUrl = router.route(this.backendUrls);
+        final String url = backendUrl + fullRequest.uri();
+        filter.filter(fullRequest, ctx);
+        //proxyService.submit(() -> fetchGet(fullRequest, ctx, url));
+        fetchGet(fullRequest, ctx, url);
+    }
+
+    private void fetchGet(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final String url) {
+        final HttpGet httpGet = new HttpGet(url);
+        //httpGet.setHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_CLOSE);
+        httpGet.setHeader(HTTP.CONN_DIRECTIVE, HTTP.CONN_KEEP_ALIVE);
+        httpGet.setHeader("mao", inbound.headers().get("mao"));
+
+        httpclient.execute(httpGet, new FutureCallback<HttpResponse>() {
+            @Override
+            public void completed(final HttpResponse endpointResponse) {
+                try {
+                    handleResponse(inbound, ctx, endpointResponse);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+            }
+
+            @Override
+            public void failed(final Exception ex) {
+                httpGet.abort();
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void cancelled() {
+                httpGet.abort();
+            }
+        });
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, final HttpResponse endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+        try {
+            byte[] body = EntityUtils.toByteArray(endpointResponse.getEntity());
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", Integer.parseInt(endpointResponse.getFirstHeader("Content-Length").getValue()));
+
+            filter.filter(response);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
